0d57be685b2703eb60e35a5af7155bd3a1f17f3f
sanyuanya <3068271240@qq.com>
2025-07-26 10:53:45 +0800

    first commit

--- config/config.go
+++ config/config.go
@@ -8,7 +8,8 @@ var (
 )
 
 func init() {
-	DATABASE_URL = getEnv("DATABASE_URL", "host=localhost user=postgres password=password dbname=doctor_db port=5432 sslmode=disable TimeZone=Asia/Shanghai")
+	// DATABASE_URL = getEnv("DATABASE_URL", "host=localhost user=postgres password=password dbname=doctor_db port=5432 sslmode=disable TimeZone=Asia/Shanghai")
+	DATABASE_URL = getEnv("DATABASE_URL", "postgresql://postgres:sanyuanya212223@@db.rtbwvdoclefhiqbteevi.supabase.co:5432/postgres")
 	JWT_SECRET = getEnv("JWT_SECRET", "Tyokb2KA5tzKqMzojpAs9k9XvaRbVddsfpvaKowKweWDRhYZBT0G06tpf1y2BQhYtfZWJQ8w3EDbXPxx0TfCHKbEWhKJH3LMPXPC33FG4etifcrrHdwdAFoxWTieHurjrxyeFrZTTc00U26A3EkkhFcqdUbJNTp4Ldwm3W9QxYfAckt4Ziydfyvw2hsdZUqt5HG471AL")
 }
 
